74797fa125e8a7d8b0f5635166cdd864db142fa2
Maicon-mk <joaomichaelfd@gmail.com>
2023-08-31 16:47:50 -0300

    EX4 Concluido

--- Ex-30-08/ex4.py
+++ Ex-30-08/ex4.py
@@ -3,8 +3,39 @@
 #Adicione métodos para adicionar notas,
 #calcular a média das notas e exibir as informações do aluno.
 
-class Studant:
-    def __init__(self, name,age, grades):
+class Student:
+    def __init__(self, name,age):
         self.name = name
         self.age = age
         self.grades = []
+
+    def __str__(self):
+        return f"Name: {self.name}\n Age: {self.age}"
+    
+    # @property
+    # def name(self):
+    #     self.__name 
+    
+    # @name.setter
+    # def name(self,name):
+    #     self.__name = name
+    
+    def add_grade(self,grade):
+        self.grades.append(grade)
+    
+    def soma(self):
+        total = sum(self.grades)
+        media = total / len(self.grades)
+        return media
+    def info (self):
+        print(
+              f"Name: {self.name}\n Age: {self.age}\n Grades: {self.grades} \n Media: {self.soma()}")
+
+name_student = input("Name student: ")
+age_student = int(input("Age student: "))
+estudant = Student(name_student,age_student)
+
+for i in range(4):
+    grade = float(input(f"Value nota {i+1}: "))
+    estudant.add_grade(grade)
+estudant.info()
---
+++ Ex-30-08/ex9.py
@@ -0,0 +1,20 @@
+#9
+#Desenvolva uma classe Bank que gerencia várias contas bancárias. 
+#Os métodos devem permitir criar novas contas, 
+#fazer transferências entre contas e calcular o saldo total do banco.
+
+
+class Bank:
+    def __init__(self, new_cont):
+        self.new_cont = new_cont
+
+
+
+class BankGerencimento:
+    def __init__(self, cal, sal, trans, total):
+        self.cal = cal
+        self.cal = sal
+        self.trans = trans
+        self.total = total
+    def cal(self):
+        pass
